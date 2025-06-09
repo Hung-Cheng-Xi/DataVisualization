8f8a365f780a00a54f545e95f85cb80bab1f8a1e
Hung-Cheng-Xi <11146062@ntub.edu.tw>
2025-06-09 15:53:32 +0800

    (refactor): remove parse_age_label function and its import

--- general/__init__.py
+++
@@ -1,5 +0,0 @@
-from .parse_age_label import parse_age_label
-
-__all__ = [
-    "parse_age_label",
-]
--- general/parse_age_label.py
+++
@@ -1,8 +0,0 @@
-# 2. 定義年齡排序函式
-def parse_age_label(label: str) -> int:
-    if "～" in label:
-        return int(label.split("～")[0])
-    elif label.endswith("+"):
-        return int(label[:-1])
-    else:
-        return int(label)
